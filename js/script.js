1c8c659b6a3c4ce2b74e65a5d081620183388db7
BK Uddin <1bkhossain@gmail.com>
2021-08-19 22:58:37 +0600

    Changed the title colors of Top Players & Top Blogs

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* //---------
+ ---------------Solution-1 -----------------
+// */
+
+document.getElementById("top-players-title").style.color = "rgb(128, 0, 128)";
+
+document.getElementById("top-blogs-title").style.color = "rgb(128, 0, 128)";
