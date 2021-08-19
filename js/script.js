37090a01e2297207de45fdb935779665a2a707a6
BK Uddin <1bkhossain@gmail.com>
2021-08-19 23:33:34 +0600

    The changed background color (rgba) of class

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@
 document.getElementById("top-players-title").style.color = "rgb(128, 0, 128)";
 
 document.getElementById("top-blogs-title").style.color = "rgb(128, 0, 128)";
+
+/* //---------
+ ---------------Solution-2 -----------------
+// */
+
+function resetColor() {
+  var playerBG = document.getElementsByClassName("player"); // get all player
+  for (var i = 0; i < playerBG.length; i++) {
+    playerBG[i].style.background = "rgba(118,1,252,0.17)";
+  }
+}
+resetColor();
